cf6792251d1a5978789c864ab1161276af62b9d1
B-pointer <bkp5@students.uwf.edu>
2016-11-12 00:38:16 -0600

    adding more buttons to battlepane

--- SEProject/src/SEProject/BattlePane.java
+++ SEProject/src/SEProject/BattlePane.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 
 public class BattlePane extends JPanel{
 	
+	private final int BUTTON_PANEL_HEIGHT= 100;
+	
 	private JPanel buttonPanel;
 	private boolean isPlayer1Turn;
 	private JButton attackButton;
@@ -32,14 +34,23 @@ public class BattlePane extends JPanel{
 	private void addButtons()
 	{
 		buttonPanel = new JPanel();
-		buttonPanel.setPreferredSize(new Dimension(640, 100));
+		buttonPanel.setPreferredSize(new Dimension(mainFrame.FRAME_WIDTH, BUTTON_PANEL_HEIGHT ));
+		
 		attackButton = new JButton("Attack");
 		attackButton.addActionListener(new AttackListener());
+		
+		healButton = new JButton("Heal");
+		healButton.addActionListener(new HealListener());
+		
+		chargeButton = new JButton("Charge");
+		chargeButton.addActionListener(new ChargeListener());
 		
 		buttonPanel.setBackground(new Color(213, 45, 216));
 		buttonPanel.setBounds(0, 402, 640, 50);
 		
 		buttonPanel.add(attackButton);
+		buttonPanel.add(healButton);
+		buttonPanel.add(chargeButton);
 		add(buttonPanel);
 		
 		a =  new Character(this);
@@ -76,5 +87,21 @@ public class BattlePane extends JPanel{
 		
 		}	
 	}
+	
+	private class HealListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("Heal");		
+		}	
+	}
+	private class ChargeListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("Charge");
+		}	
+	}
+
 
 }
