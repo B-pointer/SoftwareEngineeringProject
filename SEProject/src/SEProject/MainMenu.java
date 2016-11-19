2d76fdaee36ee7442594c73c33d4853c2284091e
B-pointer <bkp5@students.uwf.edu>
2016-11-18 21:57:55 -0600

    added exit button to main menu

--- SEProject/src/SEProject/MainMenu.java
+++ SEProject/src/SEProject/MainMenu.java
@@ -10,6 +10,7 @@ public class MainMenu extends JPanel{
 	mainFrame mFrame; //mainframe, should be set to the frame that the main menu is added to. this allows for method calls to update the panels
 	JButton StartButton; //buttons for navigation
 	JButton CharInfoButton;
+	JButton ExitButton;
 	private final int buttonLength = 100;
 	private final int buttonHeight = 40;
 	
@@ -24,14 +25,20 @@ public class MainMenu extends JPanel{
 	public void addButtons()
 	{
 		StartButton = new JButton("Start Battle");
-		StartButton.setSize(buttonLength, buttonHeight);
+		//StartButton.setSize(buttonLength, buttonHeight);
 		StartButton.addActionListener(new StartListener());
 		add(StartButton);
 		
 		CharInfoButton = new JButton("Character Info");
-		CharInfoButton.setSize(buttonLength, buttonHeight);
+		//CharInfoButton.setPreferredSize(new Dimension(buttonLength, buttonHeight));
 		CharInfoButton.addActionListener(new CharInfoListener());
 		add(CharInfoButton);
+		
+		
+		ExitButton = new JButton("Exit Game");
+		//ExitButton.setPreferredSize(new Dimension(buttonLength, buttonHeight));
+		ExitButton.addActionListener(new ExitListener());
+		add(ExitButton);
 		
 	}
 	//Listener class for the start button, sets the active panel in the main frame to battle pane, needs to be updated so that it instead navigates to the character select screen
@@ -51,6 +58,14 @@ public class MainMenu extends JPanel{
 		{
 			System.out.println("Show Character Information");
 			mFrame.setCharacterInfoPane();
+		}
+	}
+	//listener class for Exit button, closes program
+	private class ExitListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.exit(0);
 		}
 	}
 }
